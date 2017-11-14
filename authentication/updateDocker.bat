67fb50a129273b8db6ddf77123a22a10eb044017
Panos Papadopoulos <p.papadopoulos@protera.com>
2017-11-14 15:12:56 +0200

    changed config

--- authentication/updateDocker.bat
+++ authentication/updateDocker.bat
@@ -1,5 +1,5 @@
-sudo docker stop authentication
-sudo docker rm authentication
+sudo docker stop authentication-ms
+sudo docker rm authentication-ms
 sudo docker rmi flexbridge/authentication
 sudo mvn clean install -DskipTests
 sudo docker run  -p 9000:9000 -d --name authentication-ms sourcecode/authentication
