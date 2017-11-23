fc52b9e4ae2b9c2dc31ac009222ffb1fc3c0dda0
Panos Papadopoulos <p.papadopoulos@protera.com>
2017-11-23 15:06:46 +0200

    changed config

--- projects/updateDocker.bat
+++ projects/updateDocker.bat
@@ -1,6 +1,6 @@
 sudo docker stop projects-ms
 sudo docker rm projects-ms
-sudo docker rmi flexbridge/projects-ms
+sudo docker rmi sourcecode/projects-ms
 sudo mvn clean install -DskipTests
 sudo docker run  -p 9002:9002 -d --name projects-ms sourcecode/projects-ms
 
