b87d5d39728b10634fd57438ac947f451d24046f
Panagiotis <p.papadop.cs@gmail.com>
2018-02-10 14:52:29 +0200

    changed config

--- zuulkeeper-gateway/updateDocker.bat
+++ zuulkeeper-gateway/updateDocker.bat
@@ -2,5 +2,5 @@ sudo docker stop zuulkeeper-gateway
 sudo docker rm zuulkeeper-gateway
 sudo docker rmi sourcecode/zuulkeeper-gateway
 sudo mvn clean install -DskipTests
-sudo docker run  -p 443:443 -d --name zuulkeeper-gateway sourcecode/zuulkeeper-gateway
+sudo docker run  -p 1112:1112 -d --name zuulkeeper-gateway sourcecode/zuulkeeper-gateway
 
