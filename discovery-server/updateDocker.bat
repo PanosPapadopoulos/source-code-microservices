367e6129192fa3e117c4d5a560ef05d1ec9f77d4
Panagiotis <p.papadop.cs@gmail.com>
2018-03-07 11:46:31 +0200

    changed config

--- discovery-server/updateDocker.bat
+++ discovery-server/updateDocker.bat
@@ -2,5 +2,5 @@ sudo docker stop discovery-server
 sudo docker rm discovery-server
 sudo docker rmi sourcecode/discovery-server
 sudo mvn clean install -DskipTests
-sudo docker run  -p 1112:1112 -m 150M -d --name discovery-server sourcecode/discovery-server
+sudo docker run  -p 1112:1112 -m 450M -d --name discovery-server sourcecode/discovery-server
 
