b790bb50a0abe3705bbd7dca1417403fc7dd3eec
Panagiotis <p.papadop.cs@gmail.com>
2018-03-07 13:16:05 +0200

    changed config

--- authentication/updateDocker.bat
+++ authentication/updateDocker.bat
@@ -2,6 +2,6 @@ sudo docker stop authentication-ms
 sudo docker rm authentication-ms
 sudo docker rmi sourcecode/authentication
 sudo mvn clean install -DskipTests
-sudo docker run  -p 9000:9000  -m 250M -d --name authentication-ms sourcecode/authentication
+sudo docker run  -p 9000:9000 -m 250M -d --name authentication-ms sourcecode/authentication
 
 
